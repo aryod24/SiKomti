1df33bd48ab73e826cdcd9c48510a4f2442c94c4
aryod24 <aryowahyu24@gmail.com>
2024-11-22 13:31:09 +0700

    update

--- database/migrations/2024_11_11_064639_create_m_bidang_kompetensi_table.php
+++ database/migrations/2024_11_11_064639_create_m_bidang_kompetensi_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// Migration untuk tabel m_bidang_kompetensi
 return new class extends Migration
 {
     /**
@@ -15,11 +14,7 @@ return new class extends Migration
         Schema::create('m_bidang_kompetensi', function (Blueprint $table) {
             $table->id('id_kompetensi');
             $table->string('nama_kompetensi', 100);
-            $table->unsignedBigInteger('id_tugas')->nullable();
             $table->timestamps();
-
-            // Foreign key ke tabel m_jenis_tugas
-            $table->foreign('id_tugas')->references('id_tugas')->on('m_jenis_tugas')->onDelete('set null');
         });
     }
 
@@ -28,12 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        // Menghapus foreign key terlebih dahulu
-        Schema::table('m_bidang_kompetensi', function (Blueprint $table) {
-            $table->dropForeign(['id_tugas']);
-        });
-
-        // Menghapus tabel
         Schema::dropIfExists('m_bidang_kompetensi');
     }
 };
