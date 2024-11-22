fdf3d33cbc06bd79b2311abc71e88a16965d033c
aryod24 <aryowahyu24@gmail.com>
2024-11-22 13:18:09 +0700

    auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,42 @@ class AuthController extends Controller
         }
         return redirect('login');
     }
+    public function register()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+        return view('auth.register')
+            ->with('level', $level);
+    }
+    public function store(Request $request)
+    {
+        // cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id'  => 'required|integer',
+                'username'  => 'required|string|min:3|unique:m_user,username',
+                'nama'      => 'required|string|max:100',
+                'jurusan'   => 'nullable|string|max:100',
+                'ni'        => 'nullable|string|max:18',
+                'password'  => 'required|min:5'
+            ];
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                return response()->json([
+                    'status'    => false, // response status, false: error/gagal, true: berhasil
+                    'message'   => 'Validasi Gagal',
+                    'msgField'  => $validator->errors(), // pesan error validasi
+                ]);
+            }
+            UserModel::create($request->all());
+            return response()->json([
+                'status'    => true,
+                'message'   => 'Data user berhasil disimpan',
+                'redirect' => url('login')
+            ]);
+        }
+        return redirect('login');
+    }
     public function logout(Request $request)
     {
         Auth::logout();
