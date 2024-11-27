54a55345654ba8a42803d1552c3551a32c63597b
Khoirul Hidayah <rullhida23@gmail.com>
2024-11-28 03:07:11 +0700

    tampilan profil revisi fix

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,19 +1,15 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding: 120px;">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <!-- Judul Profil -->
-            <div class="text-center mb-4">
-                <h2 class="font-weight-bold" style="color: #2C3E50;">Profile Anda</h2>
-            </div>
-
+        <div class="col-12 col-md-8"> 
             <div class="card" style="background-color: #7c93b8; border-radius: 10px;">
+                <h2 class="font-weight-bold text-center" style="color: #ffffff; padding-top: 20px; padding-left: 20px; padding-right: 20px;">Profile Anda</h2>
                 <div class="card-body">
-                    <div class="d-flex align-items-center">
+                    <div class="d-flex flex-column flex-md-row align-items-center">
                         <!-- Foto Profil -->
-                        <div class="profile-picture text-center" style="flex: 1;">
+                        <div class="profile-picture text-center mb-4 mb-md-0" style="flex: 1; max-width: 200px;">
                             <img src="{{ asset('images/' . auth()->user()->avatar) }}" alt="Profile Picture" 
                                 class="rounded-circle" 
                                 style="width: 180px; height: 180px; object-fit: cover; border: 2px solid #000;">
@@ -23,7 +19,7 @@
                         </div>
 
                         <!-- Detail Profil -->
-                        <div class="profile-details ml-4" style="flex: 2;">
+                        <div class="profile-details ml-md-4 mt-4 mt-md-0" style="flex: 2; max-width: 100%; overflow: hidden;">
                             <table class="table" style="background: linear-gradient(90deg, #5c759c, #2C3E50); color: white;">
                                 <tr>
                                     <th style="border-bottom: 1px solid white;">Level</th>
@@ -47,7 +43,7 @@
                                 </tr>
                                 <tr>
                                     <th>Password</th>
-                                    <td>: *********</td>
+                                    <td>: ********</td>
                                 </tr>
                             </table>
                             <a href="{{ route('profile.update') }}" class="btn btn-primary btn-sm" style="background-color: #2C3E50; border-color: #2C3E50;">Ubah Profile dan Password</a>
@@ -58,4 +54,4 @@
         </div>
     </div>
 </div>
-@endsection   
+@endsection
