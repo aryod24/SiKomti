0e53d87f79fa3fd45146e60a901ba0c6a32ee339
aryod24 <aryowahyu24@gmail.com>
2024-12-15 23:48:22 +0700

    Modif app.php

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        Barryvdh\Snappy\ServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
