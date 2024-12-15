cf09ee971f5dc5d786318f6c684bb0a814e2cd54
aryod24 <aryowahyu24@gmail.com>
2024-12-16 00:08:11 +0700

    Level Controller + View

--- resources/views/level/create.blade.php
+++ resources/views/level/create.blade.php
@@ -8,24 +8,45 @@
         <div class="card-body">
             <form method="POST" action="{{ url('level') }}" class="form-horizontal">
                 @csrf
+                <!-- Kode Level -->
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Kode Level</label>
+                    <label for="level_kode" class="col-1 control-label col-form-label">Kode Level</label>
                     <div class="col-11">
-                        <input type="text" class="form-control" id="level_kode" name="level_kode" value="{{ old('level_kode') }}" required>
+                        <input 
+                            type="text" 
+                            class="form-control @error('level_kode') is-invalid @enderror" 
+                            id="level_kode" 
+                            name="level_kode" 
+                            value="{{ old('level_kode') }}" 
+                            required 
+                            autocomplete="off" 
+                            placeholder="Masukkan kode level (minimal 3 karakter)">
                         @error('level_kode')
                             <small class="form-text text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                 </div>
+
+                <!-- Nama Level -->
                 <div class="form-group row">
-                    <label class="col-1 control-label col-form-label">Nama Level</label>
+                    <label for="level_nama" class="col-1 control-label col-form-label">Nama Level</label>
                     <div class="col-11">
-                        <input type="text" class="form-control" id="level_nama" name="level_nama" value="{{ old('level_nama') }}" required>
+                        <input 
+                            type="text" 
+                            class="form-control @error('level_nama') is-invalid @enderror" 
+                            id="level_nama" 
+                            name="level_nama" 
+                            value="{{ old('level_nama') }}" 
+                            required 
+                            autocomplete="off" 
+                            placeholder="Masukkan nama level (maksimal 100 karakter)">
                         @error('level_nama')
                             <small class="form-text text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                 </div>
+
+                <!-- Tombol Aksi -->
                 <div class="form-group row">
                     <label class="col-1 control-label col-form-label"></label>
                     <div class="col-11">
@@ -37,7 +58,11 @@
         </div>
     </div>
 @endsection
+
 @push('css')
+<!-- Jika ada tambahan CSS khusus -->
 @endpush
+
 @push('js')
+<!-- Jika ada tambahan JS khusus -->
 @endpush
