1459fff07898fd4d9dbeb7b333b5f4c3a7a61092
Khoirul Hidayah <rullhida23@gmail.com>
2024-12-20 20:42:34 +0700

    Rev template

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -67,7 +67,6 @@
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include('layouts.breadcrumb')
 
     <!-- Main content -->
     <section class="content">
